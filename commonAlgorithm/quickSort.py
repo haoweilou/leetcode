d2da457fad709d21269d4330f4bcccf0027c1390
haoweilou <591193117@qq.com>
2021-05-31 10:30:16 +1000

    add partition in quick sort

--- commonAlgorithm/quickSort.py
+++ commonAlgorithm/quickSort.py
@@ -1,6 +1,8 @@
 import random
 from typing import List
+
 #cleanest method
+'''
 def quickSort(nums:List):
     n = len(nums)
     if n <= 1:
@@ -14,8 +16,28 @@ def quickSort(nums:List):
         else:
             greater.append(num)
     return quickSort(smaller) + [pivot] + quickSort(greater)
+'''
+#using partition
+def quickSort(nums:List,left:int,right:int):
+    if left >= right:
+        return
+    piviotIndex = partition(nums,left,right)
+    quickSort(nums,left,piviotIndex-1)
+    quickSort(nums,piviotIndex+1,right)
+
+def partition(nums:List,left:int,right:int):
+    pivot = nums[right]
+    i = left - 1
+    while left <= right:
+        if nums[left] < pivot:
+            i+=1
+            nums[left],nums[i] = nums[i],nums[left]
+        left += 1
+    nums[i+1],nums[right] = nums[right],nums[i+1]
+    return i+1
 
 
 n = [random.randint(0,100) for _ in range(10000)]
 print(n)
-print(quickSort(n))
+quickSort(n,0,len(n)-1)
+print(n)
